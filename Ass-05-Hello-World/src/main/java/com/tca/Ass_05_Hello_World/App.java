d04588d6a290914a92cac3860562241176ed29e0
aniketjumde <aniketjumde55@gmail.com>
2025-08-18 23:45:08 +0530

    intial comment

---
+++ Ass-05-Hello-World/src/main/java/com/tca/Ass_05_Hello_World/App.java
@@ -0,0 +1,10 @@
+package com.tca.Ass_05_Hello_World;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
